e9f5bb5294bfdc22e4bdc48bf43e46eb8ceeb031
admredinfun <admredinfun@gmail.com>
2019-12-30 00:07:24 -0300

    SnackBar

--- app/src/main/java/br/com/raveline/estudosnovos/fragments/ContatosFragment.java
+++ app/src/main/java/br/com/raveline/estudosnovos/fragments/ContatosFragment.java
@@ -1,6 +1,7 @@
 package br.com.raveline.estudosnovos.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,14 +9,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import br.com.raveline.estudosnovos.R;
+import br.com.raveline.estudosnovos.activities.CardViewActivity;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContatosFragment extends Fragment {
+    private Button botaoSnackBar;
 
 
     public ContatosFragment() {
@@ -27,7 +33,23 @@ public class ContatosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+        botaoSnackBar = view.findViewById(R.id.botao_snackBar_fragment_contatos);
+
+
+        //definindo Snackbar e alterando color do mesmo.
+        //Primeiro parametro é sempre uma View
+        botaoSnackBar.setOnClickListener(v ->
+
+                Snackbar.make(view,"Snackbar Pressionado",Snackbar.LENGTH_LONG).setAction("Confirmar",
+
+                        v1 -> {
+                            Intent i = new Intent(getActivity(), CardViewActivity.class);
+                            startActivity(i);
+                        }).setActionTextColor(getResources().getColor(android.R.color.darker_gray)).show());
+
+
+        return view;
     }
 
 }
